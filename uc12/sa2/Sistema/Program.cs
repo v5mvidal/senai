cc6a05ff658e8e7ea0070b2baf5f49938982df00
Manoel Vidal <manoel@v5market.com>
2022-08-30 22:30:14 -0300

    refactor: UC12/SA2 - Encontro remoto 7

--- uc12/sa2/Sistema/Program.cs
+++ uc12/sa2/Sistema/Program.cs
@@ -6,6 +6,9 @@ namespace Sistema
     {
         static void Main(string[] args)
         {
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Green;
+
             List<PessoaFisica> listaPf = new List<PessoaFisica>();
 
             static void BarraCarregamento(string texto)
@@ -31,9 +34,6 @@ namespace Sistema
 !                                                 !
 ===================================================
 ");
-
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Green;
 
             // BarraCarregamento("Iniciando ");
 
@@ -65,6 +65,8 @@ namespace Sistema
                 switch (opcao)
                 {
                     case "1":
+                        Console.Clear();
+
                         Endereco endPf = new Endereco();
 
                         Console.WriteLine("Digite seu Logradouro");
@@ -76,11 +78,14 @@ namespace Sistema
                         Console.WriteLine("Digite o Complemento da sua residência caso exista, senão, pressione ENTER para pular");
                         endPf.complemento = Console.ReadLine();
 
-                        Console.WriteLine("Este endereço é comercial? (Sim = S ou Não = N)");
-                        string endComercial = Console.ReadLine().ToUpper();
+                        string? endComercial;
 
                         do
                         {
+                            Console.WriteLine("Este endereço é comercial? (Sim = S ou Não = N)");
+
+                            endComercial = Console.ReadLine().ToUpper();
+
                             if (endComercial == "S")
                             {
                                 endPf.enderecoComercial = true;
@@ -93,19 +98,9 @@ namespace Sistema
                             }
                             else
                             {
-                                Console.WriteLine("Informe se o seu endereço é comercial com Sim = S ou Não = N");
-                                endComercial = Console.ReadLine().ToUpper();
-
-                                if (endComercial == "S")
-                                {
-                                    endPf.enderecoComercial = true;
-                                    break;
-                                }
-                                else if (endComercial == "N")
-                                {
-                                    endPf.enderecoComercial = false;
-                                    break;
-                                }
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                Console.WriteLine("A opção digitada não é válida, por favor, digite uma opção válida!");
+                                Console.ResetColor();
                             }
                         } while (endComercial != "S" || endComercial != "N");
 
@@ -122,40 +117,84 @@ namespace Sistema
                         Console.WriteLine("Digite seu salário");
                         pf.salario = float.Parse(Console.ReadLine());
 
-                        Console.WriteLine("Informe sua data de nascimento no formato [AAAA, MM, DD]");
-                        pf.dataNascimento = DateTime.Parse(Console.ReadLine());
+                        bool dataValida = false;
 
-                        bool idadeValida = pf.ValidarDataNascimento(pf.dataNascimento);
+                        do
+                        {
+                            Console.WriteLine("Digite a data de nascimento no formato [AAAA, MM, DD]");
 
-                        if (idadeValida == true)
-                        {
-                            Console.WriteLine("Cadastro aprovado");
-                            listaPf.Add(pf);
-                            Console.WriteLine(pf.PagarImposto(pf.salario));
-                        }
-                        else
-                        {
-                            Console.WriteLine("Cadastro não aprovado");
-                        }
+                            try
+                            {
+                                DateTime dataNasc = DateTime.Parse(Console.ReadLine());
+
+                                dataValida = pf.ValidarDataNascimento(dataNasc);
+
+                                if (dataValida == true)
+                                {
+                                    pf.dataNascimento = dataNasc;
+                                }
+                                else
+                                {
+                                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                                    Console.WriteLine("A data digitada não é válida, por favor, digite uma data válida!");
+                                    Console.ResetColor();
+                                }
+                            }
+                            catch (System.Exception)
+                            {
+                                Console.ForegroundColor = ConsoleColor.DarkRed;
+                                Console.WriteLine("A data digitada não é válida, por favor, digite uma data válida!");
+                                Console.ResetColor();
+                            }
+                        } while (dataValida == false);
+
+                        listaPf.Add(pf);
+
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Cadastro realizado com sucesso.");
 
                         break;
                     case "2":
-                        foreach (var cadaItem in listaPf) {
-                            Console.WriteLine($"{cadaItem.nome}, {cadaItem.cpf}");
+                        Console.Clear();
+
+                        if (listaPf.Count > 0)
+                        {
+                            foreach (PessoaFisica cadaPessoa in listaPf)
+                            {
+                                Console.Clear();
+                                Console.WriteLine(@$"
+Nome: {cadaPessoa.nome}
+Endereço: {cadaPessoa.endereco?.logradouro}, {cadaPessoa.endereco?.numero} {cadaPessoa.endereco?.complemento}
+Data de nascimento: {cadaPessoa.dataNascimento}
+Taxa de imposto a ser paga é: {cadaPessoa.PagarImposto(cadaPessoa.salario).ToString("C")}
+");
+
+                                Console.WriteLine("Pressione ENTER para continuar");
+                                Console.ReadLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Lista vazia!");
                         }
 
                         break;
                     case "3":
+                        Console.Clear();
+
                         Console.WriteLine("Digite o CPF para remover (somente números)");
-                        string cpfProcurado = Console.ReadLine();
+                        string? cpfProcurado = Console.ReadLine();
 
-                        PessoaFisica pessoaEncontrada = listaPf.Find(cadaItem => cadaItem.cpf == cpfProcurado);
+                        PessoaFisica? pessoaEncontrada = listaPf.Find(cadaItem => cadaItem.cpf == cpfProcurado);
 
-                        if (pessoaEncontrada != null) {
+                        if (pessoaEncontrada != null)
+                        {
                             listaPf.Remove(pessoaEncontrada);
-                            Console.WriteLine("A pessoa ligada ao CPF foi removida");
-                        } else {
-                            Console.WriteLine("CPF não encontrado");
+                            Console.WriteLine("A pessoa física ligada ao CPF foi removida.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("CPF não encontrado!");
                         }
 
                         break;
